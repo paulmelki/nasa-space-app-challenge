bd2896589f487810c5c997386bf7a248c602edb7
Paul Melki <33988126+paulmelki@users.noreply.github.com>
2021-09-28 08:45:15 +0200

    Create nasa_vegetation.py

---
+++ analysis_notebooks/nasa_vegetation.py
@@ -0,0 +1,17 @@
+"""
+In this script, we implement manipulation of the NASA vegetation cover data
+using geopandas
+
+:author: Paul Melki
+
+:date created: 28/09/2021
+
+"""
+
+# import required libraries 
+import cv2
+import geopandas as gpd
+import pandas as pd
+
+# read the NASA vegetation cover dataset
+
